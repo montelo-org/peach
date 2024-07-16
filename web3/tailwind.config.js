cdb1b4f85e5be71aa2e2311c121b16c0e1a29dc0
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-07-16 15:24:14 -0400

    web3 initial commit before 3js

---
+++ web3/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 10s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+      backgroundImage: {
+        noise: "url('/noise.png')",
+      },
+      colors: {
+        peach: "#FFAC40"
+      },
+      fontFamily: {
+        pacifico: ["Pacifico", "system-ui"]
+      }
+    }
+  },
+  plugins: [],
+}
+
