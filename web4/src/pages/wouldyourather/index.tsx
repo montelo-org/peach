089b1638bb250b18fc7729f1dde8240bb9d6c5a6
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-07-22 21:14:56 -0400

    would you rather page

--- web4/src/pages/wouldyourather/index.tsx
+++ web4/src/pages/wouldyourather/index.tsx
@@ -13,15 +13,10 @@ export default function WouldyouratherPage() {
 	return (
 		<main className="w-screen h-screen flex flex-col relative">
 			<div className="flex-1 bg-blue-500 flex items-center justify-center">
-				<p className="text-white text-4xl text-center px-4">{option1}</p>
+				<p className="text-white text-center px-4 font-medium leading-none text-xs w-[90%]">{option1} hello world hows it going hello world hows it going</p>
 			</div>
 			<div className="flex-1 bg-red-500 flex items-center justify-center">
-				<p className="text-white text-4xl text-center px-4">{option2}</p>
-			</div>
-			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-				<div className="bg-white rounded-full w-20 h-20 flex items-center justify-center shadow-lg">
-					<p className="text-black text-3xl font-bold">OR</p>
-				</div>
+				<p className="text-white text-center px-4 font-medium leading-none text-xs w-[90%]">{option2} hello world hows it going hello world hows it going</p>
 			</div>
 		</main>
 	);
