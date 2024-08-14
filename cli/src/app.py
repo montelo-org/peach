0304468e870f307608f25d30b32d33e6da93c90d
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-08-13 23:03:34 -0400

    wow

--- cli/src/app.py
+++ cli/src/app.py
@@ -60,6 +60,9 @@ logger.setLevel(logging.INFO)
 # libs
 ###########################################################################
 p = pyaudio.PyAudio()
+for i in range(p.get_device_count()):
+    dev = p.get_device_info_by_index(i)
+    print(f"Device {i}: {dev['name']}, Channels: {dev['maxInputChannels']}")
 stream = p.open(
     format=pyaudio.paInt16,
     channels=1,
