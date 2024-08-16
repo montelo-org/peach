502c803ec0b16fa26b4be28d87980ae48731085b
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-08-16 16:34:54 -0400

    cleanup

--- cli/src/utils.py
+++ cli/src/utils.py
@@ -20,9 +20,40 @@ def check_peach(transcription: str) -> bool:
     return bool(re.search(pattern, last_N_chars))
 
 
+def list_audio_devices(p: PyAudio):
+    info = p.get_host_api_info_by_index(0)
+    num_devices = info.get("deviceCount")
+
+    # List all audio devices with their supported sample rates
+    for i in range(0, num_devices):
+        device_info = p.get_device_info_by_index(i)
+        if (
+            device_info.get("maxInputChannels") > 0
+        ):  # Check if the device is an input device
+            print(f"Device {i}: {device_info.get('name')}")
+            try:
+                # Try to open the device with different sample rates
+                for rate in [8000, 16000, 32000, 44100, 48000]:
+                    stream = p.open(
+                        format=paInt16,
+                        channels=1,
+                        rate=rate,
+                        input=True,
+                        input_device_index=device_info["index"],
+                        frames_per_buffer=1024,
+                    )
+                    stream.close()
+                    print(f"  Supported sample rate: {rate} Hz")
+            except Exception as e:
+                print(f"  Not supported sample rate: {rate} Hz")
+    p.terminate()
+
+
 # function to get a stream and pyaudio instance
 def get_stream() -> Tuple[Stream, PyAudio]:
     input_device_index = int(os.getenv("SOUND_INPUT_DEVICE"))
+
+    list_audio_devices(p)
 
     # this is a stream that we only use for setup
     # another stream is created in worker_core that is used for recording
