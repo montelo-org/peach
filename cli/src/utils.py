333d13adfd9327227753ae9a04777c36708bf099
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-08-16 16:48:30 -0400

    w

--- cli/src/utils.py
+++ cli/src/utils.py
@@ -4,6 +4,7 @@ import re
 from typing import Tuple
 from pyaudio import PyAudio, paInt16, Stream
 import constants
+from custom_logger import logger
 
 
 # this function checks if the last N characters of the transcription contain the word "peach"
@@ -24,29 +25,15 @@ def list_audio_devices(p: PyAudio):
     info = p.get_host_api_info_by_index(0)
     num_devices = info.get("deviceCount")
 
-    # List all audio devices with their supported sample rates
+    # List all audio devices
     for i in range(0, num_devices):
         device_info = p.get_device_info_by_index(i)
         if (
             device_info.get("maxInputChannels") > 0
         ):  # Check if the device is an input device
-            print(f"Device {i}: {device_info.get('name')}")
-            try:
-                # Try to open the device with different sample rates
-                for rate in [16000, 32000, 44100, 48000]:
-                    stream = p.open(
-                        format=paInt16,
-                        channels=1,
-                        rate=rate,
-                        input=True,
-                        input_device_index=device_info["index"],
-                        frames_per_buffer=1024,
-                    )
-                    stream.close()
-                    print(f"  Supported sample rate: {rate} Hz")
-            except Exception as e:
-                print(f"  Not supported sample rate: {rate} Hz")
-    p.terminate()
+            logger.info(
+                f"Device {i}: {device_info.get('name')}, Channels: {device_info.get('maxInputChannels')},   Default Sample Rate: {device_info.get('defaultSampleRate')} Hz"
+            )
 
 
 # function to get a stream and pyaudio instance
@@ -57,7 +44,7 @@ def get_stream() -> Tuple[Stream, PyAudio]:
     # another stream is created in worker_core that is used for recording
     p = PyAudio()
     list_audio_devices(p)
-    print("Reached here")
+    logger.info("Reached here")
     stream = p.open(
         format=paInt16,
         channels=constants.CORRECT_NUM_CHANNELS,
@@ -66,7 +53,7 @@ def get_stream() -> Tuple[Stream, PyAudio]:
         frames_per_buffer=constants.CORRECT_FRAME_LENGTH,
         input_device_index=input_device_index,
     )
-    print("wow")
+    logger.info("wow")
     return stream, p
 
 
